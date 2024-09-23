ffe97b2e20ec4ffba876d9b5866fe7fc14c0c991
Pintu Kumar Giri <131274412+Pintu95045@users.noreply.github.com>
2024-09-24 00:22:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
+  
+  get width() {
+    return this._width; // Fixed the typo
+  }
+  
+  get height() {
+    return this._height; // Corrected the getter syntax
+  }
 
-class Square extends Animal {}
+  getArea() {
+    return this._width * this._height; // Returns the area of the rectangle
+  }
+}
+
+class Square extends Rectangle {
+  constructor(side) {
+    super(side, side);
+  }
+
+  getPerimeter() {
+    return 4 * this.width; 
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
