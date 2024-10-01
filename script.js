8f02738a950e74cf494f46e5bdb72bed669b26ac
Pintu Kumar Giri <131274412+Pintu95045@users.noreply.github.com>
2024-10-02 03:07:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,41 +1,31 @@
 //complete this code
+class Rectangle {}
 class Rectangle {
-constructor(width, height) {
-    this.width = width;
-    this.height = height;
-    this._width = width;
-    this._height = height;
-}
-  get widtH() {
-    return this.width;
-
-  get width() {
-    return this._width;
-}
-  get heighT() {
-    return this.height;
-
-  get height() {
-    return this._height;
+    constructor(width, height){
+        this.width=width;
+        this.height=height;
+    }
+    get getArea(){
+        return (width,height);
+    }
 }
 
-getArea() {
-    return (this.height * this.width);
-    return this._width * this._height;
-}
-}
-
-class Square extends Rectangle {
-  constructor(side) {
-    super(side, side);
-  }
-
-getPerimeter() {
-    return (4 * super.width);
-    return 4 * this.width;
-}
+class Square extends Animal {}
+class Square extends Animal {
+    constructor(width,height){
+        super();
+        this.width=width;
+        this.height=height;
+    }
+    getPerimeter(){
+        //perimeter=4*(a*a)
+        let result=4*(width*width);
+        return result;
+    }
 }
 
+let result=new Rectangle(4,5);
+console.log(result.getPerimeter);
 // Do not change the code below this line
 window.Rectangle = Rectangle;
 window.Square = Square;
